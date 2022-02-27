6b76a9858aa0ec2779189885532c16ea929be37f
acceberc <rebecca-cunha@hotmail.com>
2022-02-27 15:48:25 -0300

    Aula 03

---
+++ Aula3/ex1.py
@@ -0,0 +1,6 @@
+valor = int(input("Informe um numero: "))
+
+if valor % 3 == 0 and valor % 5 == 0:
+    print("O número é divisível por 3 e 5")
+else:
+    print("O número não é divisível por 3 e 5")
